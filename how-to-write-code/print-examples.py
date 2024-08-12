4768d5c3792d3d93268bbda6a31c0e00ae88a64f
Madison Copeland <owner@MacBook-Air.local>
2024-08-11 21:34:56 -0500

    Print Practice

--- how-to-write-code/print-examples.py
+++ how-to-write-code/print-examples.py
@@ -4,3 +4,9 @@ print("What","do","commas","do?");
 print("Does", "adding",      "space", "matter?");
 print('Launch' + 'Code');
 print("LaunchCode was founded in", 2013);
+
+print("Some Programming Languages:");
+
+print("Python\nJavaScript\nJava\nC#\nSwift");
+
+print("Python\tJavaScript\tJava\tC#\tSwift");
