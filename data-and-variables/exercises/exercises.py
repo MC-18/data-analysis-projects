3f6a3e99c6bbdbf4f09b2f5f9d7d29f96477056c
Madison Copeland <owner@MacBook-Air.local>
2024-08-12 22:14:07 -0500

    3 added

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -3,18 +3,23 @@ shuttle_name = 'Determination'
 shuttle_speed_mph = 175000
 Distance_to_Mars_km = 225000000
 Distance_to_Moon_km = 384400
-miles_per_kilometer_mkm = 0.621
+miles_per_kilometer = 0.621
+miles_to_mars = Distance_to_Mars_km * miles_per_kilometer
+hours_to_mars = miles_to_mars / shuttle_speed_mph
+days_to_mars = hours_to_mars / 24
 
 # 2. Use print() to print the 'type' each variable. Print one item per line.
 print(type(shuttle_name))
 print(type(shuttle_speed_mph))
 print(type(Distance_to_Mars_km))
 print(type(Distance_to_Moon_km))
-print(type(miles_per_kilometer_mkm))
+print(type(miles_per_kilometer))
 
 # Code your solution to exercises 3 and 4 here:
-miles_to_mars = Distance_to_Mars_km * miles_per_kilometer_mkm
-hours_to_mars = miles_to_mars / shuttle_speed_mph
-days_to_mars = hours_to_mars / 24
+print(miles_to_mars = Distance_to_Mars_km * miles_per_kilometer)
+print(hours_to_mars = miles_to_mars / shuttle_speed_mph)
+print(days_to_mars = hours_to_mars / 24)
+
+print(shuttle_name + "will take" + days_to_mars + "days to reach Mars.")
 
 # Code your solution to exercise 5 here
