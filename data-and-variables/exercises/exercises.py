ed9a2383f31f5a008fa7743d52bb041e79fbece2
Madison Copeland <owner@MacBook-Air.local>
2024-08-12 22:04:06 -0500

    exercise 3 done

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -13,5 +13,8 @@ print(type(Distance_to_Moon_km))
 print(type(miles_per_kilometer_mkm))
 
 # Code your solution to exercises 3 and 4 here:
+miles_to_mars = Distance_to_Mars_km * miles_per_kilometer_mkm
+hours_to_mars = miles_to_mars / shuttle_speed_mph
+days_to_mars = hours_to_mars / 24
 
 # Code your solution to exercise 5 here
