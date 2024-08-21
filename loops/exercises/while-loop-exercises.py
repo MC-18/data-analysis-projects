65a18f2a81e8f71057f66323458ca180805ee838
Madison Copeland <mcopeland.2018@gmail.com>
2024-08-20 20:21:00 -0500

    w3

--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -3,28 +3,21 @@ shuttle_fuel_level = 0
 num_crew_aboard = 0
 shuttle_altitude = 0
 
-
-
-
 # Exercise #1: Construct while loops to do the following:
   # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
 while shuttle_fuel_level <= 5000 or shuttle_fuel_level > 30000:
-    shuttle_fuel_level = int(input("Enter the shuttle's starting fuel level: "))
-
-
-
-
+  shuttle_fuel_level = int(input("Enter the shuttle's starting fuel level: "))
 
 # b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
 while num_crew_aboard <= 7:
-    num_crew_aboard = int(input("Enter the the number of astronauts on board: "))
+  num_crew_aboard = int(input("Enter the the number of astronauts on board: "))
 
-  
-  
-  
-# c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
-
-
+# c. Use a final loop to monitor the fuel status and the altitude of the shuttle. 
+# Each iteration, decrease the fuel level by 100 units for each astronaut aboard. 
+# Also, increase the altitude by 50 kilometers.
+while shuttle_fuel_level-100*num_crew_aboard>=0:
+  shuttle_altitude += 50
+  shuttle_fuel_level -= 100*num_crew_aboard
 
 # Exercise #2: Print the result with the phrase, The shuttle gained an altitude of ___ km and has ___ kg of fuel left. Fill in the blanks with the altitude and fuel level values.
 
