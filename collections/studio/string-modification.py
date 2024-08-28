fcb1c45c5d355f81bf7f2e167c4f2058d9b76edf
MC-18 <mcopeland.2018@gmail.com>
2024-08-27 19:52:49 -0500

    adone

--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -2,11 +2,15 @@ my_string = "LaunchCode"
 
 
 # a) Use string methods to remove the first three characters from the string and add them to the end.
-
-
+new_string = my_string[:3]
+end_string = my_string[3:]
+newest_string = end_string + new_string
+print(new_string)
+print(end_string)
+print(end_string + new_string)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
-
+print('Original string reads', my_string, 'and moving the first 3 letters to the end results in', newest_string,'.')
 
 
 # b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
