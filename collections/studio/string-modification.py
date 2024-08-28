f40022d15c37c8627758e8baaf5ca3cc5e78dfdf
MC-18 <mcopeland.2018@gmail.com>
2024-08-27 20:27:40 -0500

    bdone

--- collections/studio/string-modification.py
+++ collections/studio/string-modification.py
@@ -5,15 +5,17 @@ my_string = "LaunchCode"
 new_string = my_string[:3]
 end_string = my_string[3:]
 newest_string = end_string + new_string
-print(new_string)
-print(end_string)
 print(end_string + new_string)
 
 # Use a template literal to print the original and modified string in a descriptive phrase.
 print('Original string reads', my_string, 'and moving the first 3 letters to the end results in', newest_string,'.')
 
-
-# b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
-
-# c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+# b) Modify your code to accept user input. 
+# Query the user to enter the number of letters that will be relocated.
+user_input = input("Enter the number of letters to relocate: ")
+selection_int = int(user_input)
+altered_string = my_string[selection_int:] + my_string[:selection_int]
+print(altered_string)
+# c) Add validation to your code to deal with user inputs that are longer than 
+# the word. In such cases, default to moving 3 characters. Also, the template 
+# literal should note the error.
