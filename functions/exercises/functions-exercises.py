ec5ad5a72069c35268a0bf3f77c0b69c91360abe
MC-18 <mcopeland.2018@gmail.com>
2024-08-31 16:59:05 -0500

    part1c

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -9,18 +9,20 @@ def make_line(size):
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
-def make_square(size):
-    square = ""
-    for i in range(size):
-        square += (make_line(size) + "\n")
-    return square
-print(make_square(5))
-
-
-
+# def make_square(size):
+#     square = ""
+#     for i in range(size):
+#         square += (make_line(size) + "\n")
+#     return square
+# print(make_square(5))
 
 # Part 1 C -- Make a Rectangle
-
+def make_rectangle(width, height):
+    rectangle = ""
+    for i in range(height):
+        rectangle += (make_line(width) + "\n")
+    return rectangle
+print(make_rectangle(5, 3))
 
 
 
