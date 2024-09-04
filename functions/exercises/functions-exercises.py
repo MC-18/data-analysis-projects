a61a1ee7eea0af21dff0b04dc5701ad6ac2ae021
MC-18 <mcopeland.2018@gmail.com>
2024-09-03 19:17:20 -0500

    w/lectureupdates

--- functions/exercises/functions-exercises.py
+++ functions/exercises/functions-exercises.py
@@ -1,46 +1,43 @@
 # Part 1 A -- Make a Line
-# def make_line(size):
-#     line = ""
-#     for i in range(size):
-#         line += "#"
-#     return line
+def make_line(size):
+    line = ""
+    for i in range(size):
+        line += "#"
+    return line
 
-# print(make_line(5))
+print(make_line(5))
 
 # Part 1 B -- Make a Square
 # create a function using your make_line function to code a square
-# def make_square(size):
-#     square = ""
-#     for i in range(size):
-#         line = make_line(size)
-#         square += (make_line(size) + "\n")
-#     return square
-# print(make_square(5))
-
-
-
+def make_square(size):
+    square = ""
+    for i in range(size):
+        line = make_line(size)
+        square += (make_line(size) + "\n")
+    return square
+print(make_square(5))
 
 # Part 1 C -- Make a Rectangle
-# def make_rectangle(width, height):
-#     rectangle = ""
-#     for i in range(height):
-#         rectangle += (make_line(width) + "\n")
-#     return rectangle
-# print(make_rectangle(5, 3))
+def make_rectangle(width, height):
+    rectangle = ""
+    for i in range(height):
+        rectangle += (make_line(width) + "\n")
+    return rectangle
+print(make_rectangle(5, 3))
 
 # Making a square using make_rectangle
-# def make_square(size):
-#     return (make_rectangle(size, size))
-# print(make_square(5))
+def make_square(size):
+    return (make_rectangle(size, size))
+print(make_square(5))
 
 
 # Part 2 A -- Make a Stairs
-# def make_downward_stairs(height):
-#     stairs = ""
-#     for i in range(height):
-#         stairs += (make_line(i + 1) + "\n")
-#     return stairs
-# print(make_downward_stairs(5))
+def make_downward_stairs(height):
+    stairs = ""
+    for i in range(height):
+        stairs += (make_line(i + 1) + "\n")
+    return stairs
+print(make_downward_stairs(5))
 
 # Part 2 B -- Make Space-Line 
 # I admit to using chatGPT to help me through this. I was really struggling because i was thinking i needed to provide a 
@@ -49,7 +46,7 @@ def make_space_line(numSpaces, numChars):
     spaces = " " * numSpaces
     chars = "#" * numChars
     return spaces + chars + spaces
-# print(make_space_line(3, 5))
+print(make_space_line(3, 5))
 # Part 2 C -- Make Isosceles Triangle
 def make_isosceles_triangle(height):
     triangle = ""
@@ -57,9 +54,10 @@ def make_isosceles_triangle(height):
         triangle += (make_space_line(height - i - 1, 2 * i + 1) + "\n")
     return triangle
 
-# print(make_isosceles_triangle(5))
+print(make_isosceles_triangle(5))
 
 # Part 3 -- Make a Diamond
+# I had to look at the solution for this one!
 def make_diamond(height):
     diamond = ""
     triangle = make_isosceles_triangle(height)
@@ -69,9 +67,3 @@ def make_diamond(height):
     return diamond
 
 print(make_diamond(5))
-
-
-
-
-
-
